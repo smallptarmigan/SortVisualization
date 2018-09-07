fe8b67d7b5dc15fd59114379196ed0d1c28ada4f
akh <akhs3214@gmail.com>
2018-09-07 23:54:34 +0900

    update to generate gif

--- README.md
+++ README.md
@@ -1,25 +1,12 @@
 # SortVisualization
 
-Visualization by python3 of various sorting algorithms
+Visualization of various sorting algorithms by python3
 
 このプログラムでは様々なソートの可視化を行う
 
 python3を用いてソート時のメモリ空間を棒グラフで表示
 
-pythonのコードではフレームあたりの画像を生成するため、
-動画にする際はimagemagickを用いてgifに変換する必要
-
-コマごとの画像の生成
-```bash
-$ python3 vissort.py
-```
-出力画像はpicというディレクトリ下に出されるためない場合は
-作ってから実行するとエラーが解消されるかもしれません
-
-gif動画の生成
-```bash
-$ convert -delay 10 -loop 0 pic/image*.png movie.gif
-```
+フレームあたりの画像を生成し，imagemagickを用いてgif画像を生成する
 
 ![result](https://github.com/smallptarmigan/SortVisualization/blob/master/media/sample.gif)
 
--- vissort.py
+++ vissort.py
@@ -6,9 +6,13 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 LISTNUM = 100
 MIXNUM  = 200
+
+fig = plt.figure()
+ims = []
 
 # fixed seed value
 random.seed(0)
@@ -19,8 +23,12 @@ def mix_data(array):
         b = random.randrange(LISTNUM)
         array[a], array[b] = array[b], array[a]
 
+
+def plot(array):
+    im = plt.bar(range(len(array)), array, width=0.9, color='C0')
+    ims.append(im)
+
 def insertion_sort(array):
-    count = 0
     n = len(array)
     for i in range(1,n):
         tmp = array[i]
@@ -32,16 +40,14 @@ def insertion_sort(array):
                 if j <= 0 or tmp >= array[j-1]:
                     break
             array[j] = tmp
-        
-        fig = plt.bar(range(len(array)), array, width=0.9)
-        plt.savefig('./pic/image_{:0>6}.png'.format(count))
-        #plt.show()
-        plt.close('all')
-        count += 1
+        plot(array)
+    # result
+    for i in range(3):
+        plot(array)
 
 if __name__ == '__main__':
     data = array.array('i', range(1, LISTNUM))
     mix_data(data)
     insertion_sort(data)
-
-
+    ani = animation.ArtistAnimation(fig, ims, interval=100)
+    ani.save('sort.gif', writer='imagemagick')
