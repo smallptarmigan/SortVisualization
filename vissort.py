5bd0497cfa81eeba8cc47fe8c5a4df205601fa0b
j415828 <j415828@ecc-ext1.ecs13.info.mie-u.ac.jp>
2018-09-10 00:54:29 +0900

    add point color

--- vissort.py
+++ vissort.py
@@ -14,9 +14,16 @@ MIXNUM  = 200
 # fixed seed value
 random.seed(0)
 
-def make_image(array, count):
-    
-    plt.bar(range(len(array)), array, color='#FFA0A0')
+def make_image(array, count, a, b):
+    plt.bar(range(len(array)), array, color='#A0A0FF')
+    if (a != -1) and (b != -1):
+        mark = np.zeros(LISTNUM)
+        mark[a], mark[b] = array[a], array[b]
+        plt.bar(range(len(mark)), mark, color='#FFA0A0')
+    elif (a != -1):
+        mark = np.zeros(LISTNUM)
+        mark[a] = array[a]
+        plt.bar(range(len(mark)), mark, color='#FFA0A0')
     plt.xlim([-1,len(array)])
     plt.tick_params(labelbottom="off",bottom="off")
     plt.tick_params(labelleft="off",left="off")
@@ -24,15 +31,15 @@ def make_image(array, count):
     #plt.show()
     plt.close('all')
 
-def print_progress(now, total, par):
-    if (10 * (now / total)) > par:
-        sys.stdout.write("#")
-        par += 1
-
 def make_endimage(array, count):
     for i in range(10):
-        make_image(array, count)
+        make_image(array, count, -1, -1)
         count += 1
+
+def print_progress(now, total, par):
+    if (10 * (now / total)) > par:
+        #sys.stdout.write("#")
+        par += 1
 
 def mix_data(array):
     for i in range(MIXNUM):
@@ -45,21 +52,21 @@ def insertion_sort(array):
     par = 0
     n = len(array)
     print("insertion sort")
-    sys.stdout.write(":")
+    #sys.stdout.write(":")
     for i in range(1,n):
         tmp = array[i]
         if tmp < array[i-1]:
             j = i
             while True:
                 array[j] = array[j-1]
+                make_image(array, count, j, -1)
+                count += 1
                 j -= 1
                 if j <= 0 or tmp >= array[j-1]:
                     break
             array[j] = tmp
-        make_image(array, count)
         print_progress(i, n, par)
         #sys.exit()
-        count += 1
     print("")
     make_endimage(array, count)
 
